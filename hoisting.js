b0648a1ff6571a6e22277f16cb6d32ae30b53ece
Luxmi <luxmi5829@gmail.com>
2024-06-28 18:19:36 +0530

    first commit

---
+++ hoisting.js
@@ -0,0 +1,12 @@
+
+var a = 7;  // udefined and not defined isn't same
+
+function getName (){
+    console.log("hello js")
+}
+getName();
+
+console.log(a);
+
+
+// Hoisting: it is a phenomena in JS in which we can access the variables and functions even before initialising them .  The reason for hoisting lies in te execution context ( two phases :- memory/variable env.,  and code/thread of execution )
