09321c2832fefcf7e1992e4384a10be4a3a57b84
Luxmi <luxmi5829@gmail.com>
2024-06-29 20:42:59 +0530

    functions

---
+++ function.js
@@ -0,0 +1,17 @@
+// functions are considered as the heart of js
+
+var a =1 ;
+b();
+c();
+console.log(a);
+
+function b (){
+    var a = 10;
+    console.log(a);
+
+}
+
+function c() {
+    var a = 100;
+    console.log(a);
+}
