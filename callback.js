0f61e5a36e316f9275ad05219c78156155880d7f
Luxmi <luxmi5829@gmail.com>
2024-07-24 00:09:52 +0530

    call back

--- callback.js
+++ callback.js
@@ -1,3 +1,38 @@
-var a = 10;
+// callback fn. in JS
 
-console.log (a);
+setTimeout(function() {
+    console.log("timer");
+},3000)
+
+function x(y){
+    console.log("x");
+    y();
+}
+
+    x(function y(){
+        console.log("y")
+    })     //fn. y is the callBack fn.
+
+// JS is synchronous and single threaded language
+
+// blocking the main thread 
+
+
+// event listeners:
+function ael(){
+    let count = 0;
+    document.getElementById("click").addEventListener("click",function xyz(){
+        console.log("button clicked", count++);
+    })
+
+}
+ael();
+
+
+// power of callbacks
+
+// Garbage collectors and removeEventListeners
+
+/*
+  event listeners are heavy as it holds up the memory even when not in use
+*/
