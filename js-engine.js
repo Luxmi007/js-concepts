ba89726560f6b818e83606a58a474da5047c95e6
Luxmi <luxmi5829@gmail.com>
2024-08-14 23:37:00 +0530

    commit

--- js-engine.js
+++ js-engine.js
@@ -13,5 +13,8 @@ working of js engine:
 js takes code as input and then following happens
        step 1: Parsing :- during this, code is broken into tokensand syntax parser converts the code into AST(abstract syntax tree)
 
-       step 2: Compilation:- 
-*/
+       step 2 & 3: Compilation & Execution:- AST goes to the interpreter, converts high level code to bytecode and the code moves to execution step; while doing so it takes the help of compiler to optimise the code
+       
+       Execution step requires MEMORY HEAP and CALL STACK for its completion
+
+       */
