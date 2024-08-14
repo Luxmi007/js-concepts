20978dee38e244cf8cbcdf8257d32c5c0b4e6952
Luxmi <luxmi5829@gmail.com>
2024-08-14 23:17:50 +0530

    commit

--- js-engine.js
+++ js-engine.js
@@ -1,1 +1,17 @@
-var a = 10;
+// JS runtime environment:- it contains everrything that js needs to run the program
+/*  
+         to run js code we need:
+         JS engine, set of APIs, event loop, microtask queue
+
+         JS engine is heart of js runtime env.
+*/ 
+
+// ECMAscript is the language standard of js
+
+/* 
+working of js engine:
+js takes code as input and then following happens
+       step 1: Parsing :- during this, code is broken into tokensand syntax parser converts the code into AST(abstract syntax tree)
+
+       step 2: Compilation:- 
+*/
